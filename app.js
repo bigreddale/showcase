5e9c007fa012db0f197572c792566abb2f511ddc
Dale Gillespie <m735449@a3836594.federated.fds>
2015-07-22 17:12:57 -0700

    Foo

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-define( [ "jquery", "mcomjs/components/media/CarouselController" ], function ( $, CarouselController ) {
+define( [ "jquery", "mcomjs/components/media/CarouselController", "cookie" ], function ( $, CarouselController, Cookie ) {
+  var codes = {
+  	"1006": "3253011",
+  	"1008": "3253012",
+  	"1010": "3253013",
+  	"1012": "3253014",
+  	"1014": "3253015"  	
+  };
+  
+  var defaultCode = "3253010";
+  
+  var experiment = JSON.parse(Cookie.get("herokukey"));
+  
+  var passedCode = (null != experiment && codes[experiment["EXPERIMENT"]])?codes[experiment["EXPERIMENT"]]:defaultCode;
+  
   $.ajax( {
     type: 'GET',
     url: '/shop/catalog/product/productpool',
     data: $.param( {
         "ppCatId": 13247,
-        "ppMediaComponentId": "1420101000000000000" +"3253010",
+        "ppMediaComponentId": "1420101000000000000" + passedCode,
         "ppCustomRowId": 8
     } ),
     dataType: 'html',
